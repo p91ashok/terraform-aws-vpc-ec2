51ff04cb3395838976f8234b8376d8c0f2990b82
Ashok Pilli <p91ashok@gmail.com>
2025-12-19 11:30:42 +0530

    Format Terraform files

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-ashok-dev-002"   # ← your final bucket name
+    bucket         = "terraform-state-ashok-dev-002" # ← your final bucket name
     key            = "vpc-ec2/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-locks-dev"              # ← your final table name
+    dynamodb_table = "terraform-locks-dev" # ← your final table name
     encrypt        = true
   }
 }
